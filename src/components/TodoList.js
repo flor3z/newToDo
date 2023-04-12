89c6ea44d71f2cb730f25ff927126e90f1c75aba
Matheo <mflorez3@live.com>
2023-04-12 17:28:06 -0400

    editing and some style adjustments - pre-launch

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,38 +7,26 @@ export default class TodoList extends React.Component {
     super(props);
     this.state = {
       itemValue: '',
-      // itemSubmittedValue: '',
       itemList: [],
-      id: '',
-      // isCompleted: false,
     };
     this.onTextChangeHandler = this.onTextChangeHandler.bind(this);
-    // this.onTextClickHandler = this.onTextClickHandler.bind(this);
     this.addItem = this.addItem.bind(this);
     this.onClickComplete = this.onClickComplete.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
     this.onClickEdit = this.onClickEdit.bind(this);
   }
 
-  //************LifeCycle Methods here **************//
-  //Error was here! Once the page was Refreshed and specifically the id value loadedItems.id( which can't be used b/c Arrry's dont have Id properties)
-  //gave the id, the id based on the items "length" or position.
   componentDidMount() {
     const json = localStorage.getItem('itemList');
     const loadedItems = JSON.parse(json);
 
-    //*******continue to experience an error with Id's matching up and dupliacting key's becomes the result********
-    console.log(loadedItems);
     if (loadedItems && loadedItems.length > 0) {
       this.setState({
         itemList: loadedItems,
-        // id: loadedItems.length,
       });
     }
   }
-  //Figure Out how to send the ID to local storage so that it persists upon page refresh/revisit
   componentDidUpdate(prevProps, prevState) {
-    // console.log(this.state.itemList, this.state);
     if (prevState.itemList !== this.state.itemList) {
       const temp = JSON.stringify(this.state.itemList);
       localStorage.setItem('itemList', temp);
@@ -50,8 +38,6 @@ export default class TodoList extends React.Component {
 
     this.setState({
       [name]: value,
-
-      // itemValue: value,
     });
   }
 
@@ -69,13 +55,11 @@ export default class TodoList extends React.Component {
       const updatedList = [...this.state.itemList, newTodo];
       this.setState({
         itemList: updatedList,
-        id: uuidv4(),
         itemValue: '',
       });
     }
   }
 
-  //Keep testing (if it doesnt equal the ID it stays in original list, if it DOES equal, it gets filtered out!)
   deleteItem(id) {
     const newList = this.state.itemList.filter((item) => item.id !== id);
     this.setState({
@@ -86,17 +70,15 @@ export default class TodoList extends React.Component {
   onClickEdit(text, id) {
     const updatedTodos = this.state.itemList.map((item) => {
       if (item.id === id) {
-        console.log(item.text, text, item, item.id, id);
         return { ...item, text: text };
       } else {
         return item;
       }
     });
 
-    this.setState((prevState) => ({
+    this.setState({
       itemList: updatedTodos,
-      id: prevState.id,
-    }));
+    });
   }
 
   onClickComplete(id) {
